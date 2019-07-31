efbb69307d2dc8d7b01d0b84b8bb3bfc11b6f79f
CrisRod <crodriguez85@gmail.com>
2019-07-31 15:57:40 -0400

    Handlers

---
+++ src/controllers/groceries.handlers.ts
@@ -0,0 +1,40 @@
+export const showAllGroceries = async (req, res, next) => {
+    try {
+
+    } catch(err) {
+        next(err)
+    }
+}
+
+export const showOneGrocerie = async (req, res, next) => {
+    try {
+
+    } catch(err) {
+        next(err)
+    }
+}
+
+export const createNewGrocerie = async (req, res, next) => {
+    try {
+
+    } catch(err) {
+        next(err)
+    }
+}
+
+export const updateGrocerie = async (req, res, next) => {
+    try {
+
+    } catch(err) {
+        next(err)
+    }
+}
+
+export const deleteGrocerie = async (req, res, next) => {
+    try {
+
+    } catch(err) {
+        next(err)
+    }
+}
+
